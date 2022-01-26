e89a8b2f48b123d886c319e3b2c2901a3b629885
kadynski <krzysprive@gmail.com>
2022-01-26 13:52:37 +0100

    formatting

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,49 +1,48 @@
 import React, { useState, useEffect } from "react";
-import {BDiv, BImg} from "bootstrap-4-react";
+import { BDiv, BImg } from "bootstrap-4-react";
 import axios from "axios";
 import apiKey from "../apiKey";
 
+const Gallery = () => {
+  const [photosList, setPhoto] = useState([""]);
 
-const Gallery =()=>{
+  const options = {
+    method: "GET",
+    url: "https://api.pexels.com/v1/search",
+    mode: "cors",
+    params: { query: "hairdresser", per_page: "4", page: "1" },
+    headers: {
+      authorization: apiKey,
+    },
+  };
+  useEffect(() => {
+    axios
+      .request(options)
+      .then(function (response) {
+        setPhoto(response.data.photos);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  }, []);
 
-    const [photosList, setPhoto] = useState([""]);
-
-
-const options = {
-  method: 'GET',
-  url: "https://api.pexels.com/v1/search",
-  mode: 'cors',
-  params: {query: 'hairdresser', per_page: '4', page: '1'},
-  headers: {
-    authorization: apiKey
-  }
-};
-useEffect(() => {
-axios.request(options).then(function (response) {
-	setPhoto(response.data.photos);
-}).catch(function (error) {
-	console.error(error);
-});
-}, []);
-
-    return (
-        <BDiv>
-            <h2>galeria</h2>
-            <BDiv>
-                {photosList.map((photo)=>{
-                    return (
-                        <BImg
-                        src={photo.url}
-                        key={photo.id}
-                        id={photo.id}
-                        alt={photo.alt}
-                        />
-                    )
-                })}
-            </BDiv>
-
-        </BDiv>
-    )
+  return (
+    <BDiv>
+      <h2>galeria</h2>
+      <BDiv>
+        {photosList.map((photo) => {
+          return (
+            <BImg
+              src={photo.src.original}
+              key={photo.id}
+              id={photo.id}
+              alt={photo.alt}
+            />
+          );
+        })}
+      </BDiv>
+    </BDiv>
+  );
 };
 
 export default Gallery;
